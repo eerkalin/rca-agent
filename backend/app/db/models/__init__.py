cf7f8fd2d208e6b7b549284de6a9cc38ac0209fd
eerkalin <80304108+eerkalin@users.noreply.github.com>
2026-09-16 14:33:38 +0500

    Register application context models

--- backend/app/db/models/__init__.py
+++ backend/app/db/models/__init__.py
@@ -1,2 +1,9 @@
 from app.db.models.alert import Alert
+from app.db.models.application import (
+    Application,
+    ApplicationDependency,
+    ApplicationTool,
+    Connection,
+    DependencyTool,
+)
 from app.db.models.investigation import Investigation
